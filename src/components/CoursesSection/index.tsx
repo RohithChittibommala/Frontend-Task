28e7c33c143f8f49e86e30409df635d6d91fd0d2
Rohith ch <myselfrohith@outlook.com>
2023-08-25 17:42:53 +0530

    pass courses to courses section as props

--- src/components/CoursesSection/index.tsx
+++ src/components/CoursesSection/index.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import styles from "./coursesSection.module.css";
-import courses from "../../data/courses";
 import Course from "./Course";
-import Carousel from "../Carousel";
+import { ICourse } from "../../types/CourseType";
+import styles from "./coursesSection.module.css";
 
-function CoursesSection() {
+interface ICoursesSectionProps {
+  courses: ICourse[];
+}
+
+function CoursesSection(props: ICoursesSectionProps) {
   return (
     <section className={styles["courses-section"]}>
       <h2>Checkout my courses </h2>
       <div className={styles["courses-container"]}>
-        {courses.map((course) => (
+        {props.courses.map((course) => (
           <Course key={course.id} {...course} />
         ))}
       </div>
