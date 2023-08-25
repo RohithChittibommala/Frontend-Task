d5a262a3402dbafd0fcc2b31995c8fbdc6c4392b
Rohith ch <myselfrohith@outlook.com>
2023-08-25 21:10:05 +0530

    add responsive styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import CoursesSection from "./components/CoursesSection";
+import BootcampSection from "./components/BootcampSection";
+import bootcamps from "./data/bootcamps";
+import Footer from "./components/FooterSection";
+import courses from "./data/courses";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
       <HeroSection />
-      <CoursesSection />
+      <CoursesSection courses={courses} />
+      <BootcampSection tabs={bootcamps} />
+      <Footer />
     </>
   );
 }
