c4db778efbe31994f8d53c0113fbe29b66587f0c
Rohith ch <myselfrohith@outlook.com>
2023-08-25 17:44:29 +0530

    refactor herosection by passing banner as element

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -2,16 +2,21 @@ import styles from "./heroSection.module.css";
 import banners from "../../data/banner";
 import Carousel from "../Carousel";
 import stats from "../../data/stats";
+import Banner from "../Banner";
 
 function HeroSection() {
+  const Banners = banners.map((banner, ind) => (
+    <Banner key={banner.id} {...banner} />
+  ));
+
   return (
     <section className={styles["hero-section"]}>
       <div className={styles["banner-section"]}>
-        <Carousel banners={banners} />
+        <Carousel elements={Banners} />
       </div>
       <div className={styles["stats-section"]}>
         {stats.map((stat) => (
-          <div className={styles["stat"]}>
+          <div key={stat.id} className={styles["stat"]}>
             <img src={stat.imgUrl} alt="hat" />
             <p>{stat.description}</p>
           </div>
