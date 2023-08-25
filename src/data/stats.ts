78d625f18a118f6e9cc395ed61a5b28a1e8a353c
Rohith ch <myselfrohith@outlook.com>
2023-08-25 17:45:45 +0530

    add mock stats data

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -1,13 +1,16 @@
 const stats = [
   {
+    id: 1,
     imgUrl: "/stat1.svg",
     description: "There are 50+ happy customers who have taken this course",
   },
   {
+    id: 2,
     imgUrl: "/stat2.svg",
     description: "There are 50+ happy customers who have taken this course",
   },
   {
+    id: 3,
     imgUrl: "/stat3.svg",
     description: "There are 50+ happy customers who have taken this course",
   },
