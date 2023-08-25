9b90047c495d15ce224d56e0af55f5b3bfd709fe
Rohith ch <myselfrohith@outlook.com>
2023-08-25 17:45:03 +0530

    add mock course data

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -5,21 +5,21 @@ const courses: ICourse[] = [
     id: 1,
     title: "Software Development",
     description:
-      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack)",
+      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack) Learn to build entire web applications from start to finish on",
     imgUrl: "/course1.svg",
   },
   {
     id: 2,
     title: "Best Data Analytics Course",
     description:
-      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack)",
+      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack) Learn to build entire web applications from start to finish on",
     imgUrl: "/course2.svg",
   },
   {
     id: 3,
     title: "Software Development",
     description:
-      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack)",
+      "Learn to build entire web applications from start to finish on one of the most versatile tech stacks : MongoDB, Express.js, React.js and Node.js (MERN stack) Learn to build entire web applications from start to finish on",
     imgUrl: "/course1.svg",
   },
 ];
